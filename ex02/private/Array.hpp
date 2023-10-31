5e3b73e9ea0811f47949afd20f9463882b49b574
Lisa <lduheron@e3r8p4.42.fr>
2023-10-31 19:07:11 +0100

    In ex03: creating array class

---
+++ ex02/private/Array.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.hpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 22:12:15 by lduheron          #+#    #+#             */
+/*   Updated: 2023/10/27 15:37:58 by lduheron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ARRAY_HPP
+# define ARRAY_HPP
+
+# include <iostream>
+# include <string>
+# include <limits.h>
+
+#define SUCCESS 0
+#define ERROR 1
+
+template<typename T>
+class Array
+{
+	private:
+		T*	_type;
+		int	_size;
+
+	public:
+		Array<T>();
+		Array<T>(Array<T> const & src);
+		~Array<T>();
+
+		Array &	operator=(Array const & rhs);
+
+		std::string const &	getSize(void) const;
+
+		// int	size(void)
+		// {return this->_size};
+};
+
+std::ostream & operator<<(std::ostream & lhs, Array const & rhs);
+
+#endif
---
+++ ex02/src/class/Array.cpp
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Array.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lduheron <lduheron@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 12:12:32 by lduheron          #+#    #+#             */
+/*   Updated: 2023/10/27 15:37:30 by lduheron         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "Array.hpp"
+
+// Constructor -----------------------------------------------------------------
+
+Array::Array()
+{
+	// std::cout << "Default constructor called.\n";
+}
+
+Array::Array(Array const & src)
+{
+	// std::cout << "Copy constructor called.\n";
+}
+
+// Destructor ------------------------------------------------------------------
+
+Array::~Array()
+{
+	// std::cout << "Destructor called.\n";
+}
+
+// Accessors -------------------------------------------------------------------
+
+std::string const &	Array::getSize(void) const
+{
+	return (this->_size);
+}
+
+// Overload --------------------------------------------------------------------
+
+Array &	Array::operator=(Array const & rhs)
+{
+	this->_grade = rhs._grade;
+	return *this;
+}
+
+std::ostream & operator<<(std::ostream & lhs, Array const & rhs)
+{
+	lhs << "Size " << rhs.getSize() << ".\n";
+	return lhs;
+}
+
+// Functions -------------------------------------------------------------------
